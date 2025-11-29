bd379d92a4fe0892cdfceee490b2009a752849a5
Michael <crazedmonkey231@users.noreply.github.com>
2025-11-29 14:44:40 -0500

    adjusted multiplayer example

--- src/levels/MultiplayerLevel.ts
+++ src/levels/MultiplayerLevel.ts
@@ -4,15 +4,18 @@ import { Level } from "../Level";
 
 /** A template level class to be copied and customized */
 export class MultiplayerLevel extends Level {
-  roomId: string = "lobby";
+  roomId: string = "sandbox";
   playerName: string = "Player";
   players: Map<any, THREE.Mesh> = new Map();
   space: Phaser.Input.Keyboard.Key | undefined;
+  socketId: any;
   syncTimer: number = 0;
   moveSpeed: number = 0.1; // units per second
   playerPosition: THREE.Vector3 = new THREE.Vector3();
   playerRotation: THREE.Euler = new THREE.Euler();
   playerScale: THREE.Vector3 = new THREE.Vector3(1, 1, 1);
+  levelThings: Map<string, THREE.Mesh> = new Map();
+  private tmpVec: THREE.Vector3 = new THREE.Vector3();
   constructor(baseScene: GameScene) {
     super(baseScene);
   }
@@ -38,6 +41,10 @@ export class MultiplayerLevel extends Level {
 
     this.playerPosition.set(randX, 0, randZ);
 
+    this.initializeMultiplayer();
+  }
+
+  initializeMultiplayer(): void {
     const mgr = this.gameScene.initMultiplayer(this.roomId, {
       name: this.playerName,
       score: 0,
@@ -51,9 +58,14 @@ export class MultiplayerLevel extends Level {
 
     mgr.on("init", (data: any) => {
       console.log("Multiplayer init data:", data);
+      this.socketId = data.you;
       const players: Record<string, any> = data.players;
       for (const id in players) {
         this.addPlayer(id, players[id]);
+      }
+      const things = data.things;
+      for (const thingId in things) {
+        this.initThingFromData(thingId, things[thingId]);
       }
     });
 
@@ -75,13 +87,63 @@ export class MultiplayerLevel extends Level {
       const mesh = this.players.get(data.id);
       if (mesh) {
         const position = data.position;
-        mesh.position.set(
-          position.x,
-          position.y,
-          position.z
-        );
+        this.tmpVec.set(position.x, position.y, position.z);
+        mesh.position.lerp(this.tmpVec, 0.5);
       }
     });
+
+    this.gameScene.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
+      // this.gameScene.input.mouse?.requestPointerLock();
+      const randX = Math.random() * 20 - 10;
+      const randZ = Math.random() * 20 - 10;
+      mgr.emit("spawnThing", {
+        id: `${this.socketId}_thing_${Date.now()}`,
+        type: "cube",
+        transform: {
+          position: {
+            x: this.playerPosition.x + randX,
+            y: this.playerPosition.y,
+            z: this.playerPosition.z + randZ,
+          },
+          rotation: {
+            _x: 0,
+            _y: 0,
+            _z: 0,
+            isEuler: true,
+          },
+          scale: {
+            x: 1,
+            y: 1,
+            z: 1,
+          },
+        },
+      });
+    });
+
+    mgr.on("thingSpawned", (data: any) => {
+      console.log("Thing spawned:", data);
+      this.initThingFromData(data.id, data);
+    });
+
+    mgr.on("thingDespawned", (thingId: string) => {
+      console.log("Thing despawned:", thingId);
+      const mesh = this.levelThings.get(thingId);
+      if (mesh) {
+        this.remove(mesh);
+        this.levelThings.delete(thingId);
+      }
+    });
+
+    // this.gameScene.input.on("pointermove", (pointer: Phaser.Input.Pointer) => {
+    //   if (pointer.isDown) {
+    //     this.playerRotation.y -= pointer.movementX * 0.002;
+    //     this.playerRotation.x -= pointer.movementY * 0.002;
+    //     this.playerRotation.x = Math.max(
+    //       -Math.PI / 2,
+    //       Math.min(Math.PI / 2, this.playerRotation.x)
+    //     );
+    //   }
+    // });
   }
 
   addPlayer(id: any, data: any): void {
@@ -106,6 +168,33 @@ export class MultiplayerLevel extends Level {
     mesh.scale.set(transform.scale.x, transform.scale.y, transform.scale.z);
     this.add(mesh);
     this.players.set(id, mesh);
+    mesh.userData.playerId = id; // Store playerId for reference
+  }
+
+  initThingFromData(
+    thingId: string,
+    data: any
+  ): void {
+    // Example: create a simple cube thing based on data
+    console.log("Initializing thing from data:", thingId, data);
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const mesh = new THREE.Mesh(geometry, material);
+    const transform = data.transform;
+    mesh.position.set(
+      transform.position.x,
+      transform.position.y,
+      transform.position.z
+    );
+    mesh.rotation.set(
+      transform.rotation._x,
+      transform.rotation._y,
+      transform.rotation._z
+    );
+    mesh.scale.set(transform.scale.x, transform.scale.y, transform.scale.z);
+    this.add(mesh);
+    mesh.userData.thingId = thingId; // Store thingId for reference
+    this.levelThings.set(thingId, mesh);
   }
 
   update(time: number, dt: number, args: any) {
